dff602b6871f6320f58b1102e33b65228f4ca8d3
Melissa Aybar <30645979+Tamiyo22@users.noreply.github.com>
2019-04-29 22:43:50 -0500

    completed

--- bitcoinChallenge.js
+++ bitcoinChallenge.js
@@ -8030,3 +8030,28 @@ var bitcoinData = [
   "activeAddresses": null
 }
 ]
+
+//What is the data type of bitcoinData? Let’s say we just want to look at bitcoin information on May 1st, 2013. How can we do that?
+
+bitcoinData.filter((time)=> time.date === "2013-05-01" )
+
+// Let’s say we want to create an array containing only the date and price of each day. Use the built-in map method to create an array containing that information.
+
+bitcoinData.map((info)=> `Date:${info.date} Price:${info['price(USD)']}`)
+
+// Let’s create an array that only includes days when exchange volume was not 0. Which method should we use?
+bitcoinData.filter((time)=> time["exchangeVolume(USD)"]!== 0 ).map((days)=> days.date)
+
+// Everyday, new Bitcoins are generated. If we want to find out how many were generated all together during the time that’s provided in the dataset, can you figure out how to calculate it using reduce?
+
+bitcoinData.map((coins)=> coins.generatedCoins).reduce((total,coins)=> total + coins)
+
+// 	Bitcoin price fluctuates a lot. Let’s combine some array methods to find the total number of days when bitcoin price was over $100!
+
+bitcoinData.filter((cost)=> cost["price(USD)"] > 100)
+
+// 	 And finally, let’s find the average bitcoin transaction fees between 2013 and 2015. Be sure to floor this value.
+
+var totalFees=bitcoinData.map((fee)=> fee.fees).reduce((total,cost)=> total + cost)
+Math.floor(totalFees)
+//15716
